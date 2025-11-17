797944fc946fd3bd5cb079babb3587035a3308a0
Rebeca <rebecagomesdev@gmail.com>
2025-11-17 13:04:16 -0300

    Revert "Apps criados:"
    This reverts commit 2a34434208badfd03b83d5c225c5ef069309c83b.

--- hidromanager/settings.py
+++ hidromanager/settings.py
@@ -37,12 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'usuarios',
-    'fornecedores',
-    'produtos',
-    'estoque',
-    'compras',
-    'retiradas',
 ]
 
 MIDDLEWARE = [
@@ -126,10 +120,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
-#AUTH_USER_MODEL especifica o modelo de usuário que será utilizado no projeto.
-#Neste caso, estamos usando um modelo customizado chamado 'Usuario' definido no app 'usuarios'.
-#Isso permite adicionar campos personalizados como tipo de usuário, telefone, endereço e status de ativação.
-AUTH_USER_MODEL = 'usuarios.Usuario'
--- usuarios/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Usuario
-
-class CadastroFuncionarioForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-
-    class Meta:
-        model = Usuario
-        fields = ['username', 'email', 'telefone', 'endereco', 'password']
